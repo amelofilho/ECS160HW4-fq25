02163dc7ef97b7dcd82c81df5a889142ae4e6a24
Adriano Melo Filho <adrianomelo1997@gmail.com>
2025-12-11 11:41:56 -0800

    Update: microservices pom.xml Setup springboot dependency

--- microservices/src/main/java/com/ecs160/App.java
+++ microservices/src/main/java/com/ecs160/App.java
@@ -1,13 +1,21 @@
 package com.ecs160;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        Launcher myLauncher = new Launcher();
-        int port = 8000;
-        myLauncher.launch(port);
+        // Launcher myLauncher = new Launcher();
+        // int port = 8000;
+        // myLauncher.launch(port);
         // summarize the issue call llm
         //System.out.println( "Starting Server on port" + port + "!" );
+
+        SpringApplication.run(App.class, args);
+
+
     }
 }
